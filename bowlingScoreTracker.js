5e041672127acade56e782f6f3aaaa62894e3220
Aditya Sinha <adityasinha361@gmail.com>
2023-01-18 12:40:21 +0530

    feat: bowling score calculator for open and spare cases

---
+++ bowlingScoreTracker.js
@@ -0,0 +1,40 @@
+const frames = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+const frames2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+const frames3 = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function trackScore(frames) {
+  let score = 0;
+  let temp = frames.length == 20 ? 2 : 3;
+
+  for (let i = 0; i < frames.length - temp; i += 2) {
+    let frame = [];
+
+    frame.push(frames[i]);
+    frame.push(frames[i + 1]);
+    let sum = 0;
+    for (let j = 0; j < 2; j++) sum += frame[j];
+
+    // open frame case
+    if (sum < 10) {
+      score += sum;
+    }
+
+    // spare case
+    else if (sum == 10 && frames[i] != 10) {
+      score += sum + frames[i + 2];
+    }
+
+    // strike case
+    // else if (score == 10 && frames[i + 1] == 0) {
+
+    // }
+  }
+
+  // calculating for last frame
+  if (frames.length == 20) score = score + frames[18] + frames[19];
+  else score = score + frames[18] + frames[19] + frames[20];
+
+  return score;
+}
+
+console.log(trackScore(frames3));
